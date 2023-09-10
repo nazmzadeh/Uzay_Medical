8c697566ec56578f2d94b2e121e527b666d5a773
Naz Mzadeh <7QD1XHL@code.edu.az>
2023-09-11 02:52:12 +0400

    updated product details

--- js/main.js
+++ js/main.js
@@ -213,8 +213,8 @@ if (form) {
 
 function toggleMenu() {
   const menu = document.querySelector('.menu');
-  menu.classList.toggle('show');
+  if (menu) menu.classList.toggle('show');
 }
 
 const menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', toggleMenu);
+if (menuButton) menuButton.addEventListener('click', toggleMenu);
